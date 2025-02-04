8793bd6f64fa0d1250655ca06167de018fbdf241
Ahmed Abuelbasher <abuelbasheribrahim.a@northeastern.edu>
2025-02-04 17:17:38 -0500

    final commit

--- todolist.py
+++ todolist.py
@@ -1,42 +1,92 @@
 tasks =[]
 taskstatus=[]
-decision=input("Would you like to, Add tasks (add) , view tasks( view) , mark task as complete(mark), or delete tasks(delete) ")
+
 x=2
-while x==2:
-    if decision=='add':
-        task=input('what is the task')
-        taskstatus.append('ongoing')
-        tasks.append(task)
-        decision=input("Would you like to, Add tasks (add) , view tasks( view) , mark task as complete(mark), or delete tasks(delete) ")
+
+def addfunc():
+    task=input('what is the task')
+    taskstatus.append('ongoing')
+    tasks.append(task)
     
     
-    elif decision== 'view':
-        for i in range(1+ len(tasks)):
-            
-            
-            
-            
+def viewfunc():
+    if len(tasks)==0:
+        print("No Ongoing Tasks!!!")
+    else:
+        for i in range(0, len(tasks)):
             print("Task #", i+1,": ",tasks[i], taskstatus[i])
-            decision=input("Would you like to, Add tasks (add) , view tasks( view) , mark task as complete(mark), or delete tasks(delete) ")
+    
+def completetask():
+    
+    for i in range(0, len(tasks)):
+        print("Task #", i+1,":",tasks[i])
             
-    elif decision == 'mark':
-        for i in range(len(tasks)):
-            print("Task #", i+1,":",tasks[i])
-        completed= input("which tasks have you completed ")
-        taskstatus[i]='COMPLETE'
+            
+    completed=int( input("which tasks have you completed "))
+    taskstatus[completed-1]='COMPLETE'
+    
+def deletetask():
+    for i in range(0, len(tasks)):
+        print("Task #", i+1,": ",tasks[i], taskstatus[i])
+    deleted=int(input("which task would you like to delete"))
+    tasks.pop(deleted-1)
+    taskstatus.pop(deleted-1)
+    
+    
+
+def decisionfun():
+    decision=input("Would you like to, Add tasks (add) , view tasks( view) , mark task as complete(mark), or delete tasks(delete) ")
+
+            
+
+    if (decision!='add') and (decision!='view') and (decision!='mark') and (decision!='delete'):
+        decision= input("Please try inputting again")
+    else:
+        decision=str(decision)
+                
+        return decision
+            
+    
+def main():
+
     
     
     
-print(ghhgjh)
+    while x==2:
+        decision=decisionfun()
+        
+        if decision =='add':
+            addfunc()
+            
+            
+
+            
+
+        elif decision== 'view':
+            viewfunc()
+            
+           
+
+            
        
-        
-        #testing if it updates 
+        elif decision == 'mark':
+            completetask()
+           
+           
+
             
+        
+        elif decision=='delete':
+            deletetask()
+        
     
+
+            
         
     
     
     
     
+if __name__ == "__main__":
     
-    
+    main()
